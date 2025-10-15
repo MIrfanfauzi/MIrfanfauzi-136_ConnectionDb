edf95fe2d8ae6e2680da65488b93fc81d4a1f412
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-10-15 12:07:28 +0700

    Inisialisasi Server Express

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(bodyParser.json());
+
